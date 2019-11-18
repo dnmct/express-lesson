ded7483ac95e7b104cfe1e78919a564ea981fdfe
Dan McAtee <info@danmcatee.com>
2019-11-18 07:47:02 +0100

    add comments

--- src/index.js
+++ src/index.js
@@ -4,16 +4,20 @@ const morgan = require("morgan");
 
 const app = express();
 
+// applying middleware
 app.use(json());
 app.use(morgan("dev"));
 
+//custom middleware
 const log = (req, res, next) => {
   console.log("logging");
   next();
 };
 
+// apply custom middleware
 app.use(log);
 
+// exact matching
 app.get("/", (req, res, next) => {
   res.send({ message: "hello" });
 });
@@ -23,10 +27,12 @@ app.post("/", (req, res) => {
   res.send({ message: "ok" });
 });
 
+// Parameter matching :id is a wildcard which can be accessed through req.params.id
 app.get("/project/:id", (req, res) => {
   res.send({ projectId: req.params.id });
 });
 
+// exact matching
 app
   .route("/data")
   .get((req, res) => {
@@ -39,3 +45,5 @@ app
 app.listen(3000, () => {
   console.log("Server is running");
 });
+
+// run with node src/index.js when in the root project folder
